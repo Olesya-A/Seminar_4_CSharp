7c32b7cbe3c9c5f59e6d5c2b91da4215136c8a3d
Olesya <lesusha1@yandex.ru>
2022-09-16 01:44:47 +0300

    add Task_28

---
+++ Task_28/Program.cs
@@ -0,0 +1,18 @@
+﻿// 28: Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(CalculateFactorial(n));
+
+int CalculateFactorial(int N)
+{
+    int factorial = 1;
+    for (int i = 2; i <= N; i++)
+        factorial = factorial * i;
+    return factorial;
+}
